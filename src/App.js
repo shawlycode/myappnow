38926cb48419b1d7e65cc5d686cdec51fbfb00d3
Shawlycode <kutinaorlando@gmail.com>
2021-09-30 12:13:12 +0000

    Added a fragment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
-import Counter from './Counter';
+import Counter from '../src/Counter';
+
 
 function App() {
   return (
-    <div>
+    
+    <>
      <Counter />
-    </div>
+    </>
+    
   );
 }
 
